62c77e7200ecbea16ffce19e023084df2c841830
Himanshu88027 <himanshukumar88027@gmail.com>
2024-08-03 14:37:08 +0530

    channel profile route fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { forgotPassword, getCurrentUser, getWatchHistory, login, logout, refreshAccessToken, updateUser, updateUserAvatar, updateUserCoverImage, userRegister } from "../controllers/user.controller.js";
+import { forgotPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, login, logout, refreshAccessToken, updateUser, updateUserAvatar, updateUserCoverImage, userRegister } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { jwtVerify } from "../middlewares/auth.middleware.js";
 
@@ -26,12 +26,12 @@ router.route("/logout").post(jwtVerify, logout)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/forgot-password").post(jwtVerify, forgotPassword)
 router.route("/current-user").get(jwtVerify, getCurrentUser)
-router.route("/update-user").post(jwtVerify, updateUser)
+router.route("/update-user").patch(jwtVerify, updateUser)
 
 router.route("/avatar").patch( jwtVerify, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch( jwtVerify, upload.single("coverImage"), updateUserCoverImage)
 
-router.route("/channel/:username").get(updateUser)
+router.route("/channel/:username").get(getUserChannelProfile)
 router.route("/history").get(jwtVerify, getWatchHistory)
 
 export default router;
