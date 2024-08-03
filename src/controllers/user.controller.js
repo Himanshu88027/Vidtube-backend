331de3b3e71b51ae01aa2fe1dd0e60a22f408bcc
Himanshu88027 <himanshukumar88027@gmail.com>
2024-08-03 13:27:42 +0530

    User Watch History controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { deleteOnCloudinary, uploadOnCloudinary } from "../utils/cloudinaryService.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async (user_id) => {
     try {
@@ -427,6 +428,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id),
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project:{
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                owner:{
+                    $first: "$owner"
+                }
+            }
+        }
+    ])
 
+    if (!user?.length) {
+        throw new ApiError(404, "User not found")
+    }
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user[0].watchHistory, "User watch history")
+    )
+})
 
-export { userRegister, login, logout, refreshAccessToken, forgotPassword, getCurrentUser, updateUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile }
+export { userRegister, login, logout, refreshAccessToken, forgotPassword, getCurrentUser, updateUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory }
